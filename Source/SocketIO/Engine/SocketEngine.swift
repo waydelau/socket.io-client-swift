b3592b8ca472556b7b6821360da4d1ca4e0046ee
liuwenxuan <waydelau@qq.com>
2023-07-19 11:19:04 +0800

    Merge pull request #1 from wmasilva/mergedPR904
    Merged pr904

--- Source/SocketIO/Engine/SocketEngine.swift
+++ Source/SocketIO/Engine/SocketEngine.swift
@@ -742,7 +742,7 @@ extension SocketEngine {
     /// - Parameters:
     ///   - event: WS Event
     ///   - _:
-    public func didReceive(event: WebSocketEvent, _: WebSocket) {
+    public func didReceive(event: WebSocketEvent, _: WebSocketClient) {
         switch event {
         case let .connected(headers):
             wsConnected = true
